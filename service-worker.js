98d669ba3fd2830da6b1f7c9a809f9ccd80ac4d7
tstavinues-source <tstavinues@gmail.com>
2026-06-14 02:48:38 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "catatan-finansial-v1";
+
+const BASE_PATH = "/Catatan-Finansial/";
+
+const urlsToCache = [
+  BASE_PATH,
+  BASE_PATH + "index.html",
+  BASE_PATH + "style.css",
+  BASE_PATH + "script.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
