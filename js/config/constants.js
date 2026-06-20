bf7d71dc349415456497861e3946feeb01cc6cf5
tstavinues-source <tstavinues@gmail.com>
2026-06-20 20:06:00 +0900

    Create constants.js for system configuration
    Added constants for Firebase and application configuration.

---
+++ js/config/constants.js
@@ -0,0 +1,24 @@
+/**
+ * System Configuration & Constants
+ * Menyimpan parameter tetap agar mudah dikelola dalam satu file.
+ */
+
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDuGNM793lZOUJEX_LAEaxCipFOw6TT35E",
+    authDomain: "agrivision-574be.firebaseapp.com",
+    [span_0](start_span)databaseURL: "https://agrivision-574be-default-rtdb.firebaseio.com", // <-- Sudah bersih dari format Markdown[span_0](end_span)
+    projectId: "agrivision-574be",
+    storageBucket: "agrivision-574be.firebasestorage.app",
+    messagingSenderId: "732120986243",
+    appId: "1:732120986243:web:d025c9a2908b1ca892a1b6"
+};
+
+export const APP_CONFIG = {
+    LEDGER_NODE: 'aurafi_ledger',
+    VERSION: '3.2.6',
+    DEFAULT_CURRENCY: 'JPY',
+    DEFAULT_THEME: 'midnight',
+    THROTTLE_MS: 300,        
+    MAX_RETRY_AI: 3,         
+    CACHE_TTL_MS: 60000      
+};
